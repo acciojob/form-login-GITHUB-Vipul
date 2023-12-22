a4cd67f1988c0b60b67d128a10188a86d0a83551
GITHUB-Vipul <126069867+GITHUB-Vipul@users.noreply.github.com>
2023-12-23 00:10:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
+	//event.preventDefault();
+	let form=document.getElementById("form1");
+	let fname=form.fname.value;
+	let lname=form.lname.value;
+	alert(`${fname}`+" "+`${lname}`);
 
 }
